96d9e3b7371c2becb4444bb91108df772302357e
KirstenHagenaars <49717068+KirstenHagenaars@users.noreply.github.com>
2019-12-03 14:48:38 +0100

    reading file works

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import pandas as pd
-import os
 
 
 def load_data():
-    data = pd.read_csv("dataset_24_mushrooms.csv")
+    data = pd.read_csv("dataset_24_mushroom.csv")
     print(data.head())
 
 
 def main():
-    #load_data()
-    print("a")
-    #print(os.getcwd())
+    load_data()
+
+main()
