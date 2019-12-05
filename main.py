02549107ea8bcab81346cd794581062c524003ad
hellena55 <ellen.gunnars@gmail.com>
2019-12-05 16:32:52 +0100

    Two util functions and worked on impurity()

--- main.py
+++ main.py
@@ -9,55 +9,110 @@ classes = []
 
 def load_data():
     global data
-    data = pd.read_csv("dataset_24_mushroom.csv")           #panda dataframe
-    print(data.head())
+    data = pd.read_csv("dataset_24_mushroom.csv")  # panda dataframe
+    # print(data.head())
 
 
 def gini_index(total, inClassA, inClassB):
-    gini = 1 - ((inClassA/total)^2 + (inClassB/total)^2)
+    gini = 1 - ((inClassA / total) ** 2 + (inClassB / total) ** 2)
     if 0 < gini < 1:
         return gini
     else:
         print("Gini index problem")
+        return 0
 
 
+def determineLabels(array):  # Finds the different classes of an attribute
+    labels = []
+    seen = []
+
+    for label in array:
+        there = False
+        for s in seen:
+            if s == label[1]:
+                there = True
+        if not there:
+            labels.append(label[1])
+            seen.append(label[1])
+    return labels
+
+
+def countInstances(instances, attribute, labels):  # Counts the number of instances of diff.classes within an attribute
+    counted = [0 for x in range(0, len(labels))]
+
+    for i in range(0, len(instances)):
+        for label in labels:
+            index = labels.index(label)
+            if instances.iloc[i][attribute][1] == label:
+                counted[index] = counted[index] + 1
+                break
+    return counted
+
+
+# TODO finish function
 def impurity(instances):
     global classes
     inClassA, inClassB = 0, 0
 
+    # TODO use countInstances here
     # Find number of instances of each class
-    for i in instances:
-        if i["class"] == classes[0]:
+    for i in range(0, len(instances)):
+        if instances.iloc[i]["class"][1] == classes[0]:
             inClassA = inClassA + 1
-        elif i["class"] == classes[1]:
+        elif instances.iloc[i]["class"][1] == classes[1]:
             inClassB = inClassB + 1
         else:
             print("Unexpected class in function impurity()")
 
+    print(inClassA)
+    print(inClassB)
+
     # Compute gini index
-    gini_index(len(instances), inClassA, inClassB)
+    gini = gini_index(len(instances), inClassA, inClassB)
 
     # Test all possible splits
-        #CODE...
+
+    for attribute in instances.columns:
+        labels = determineLabels(instances[attribute])
+
     # Weight branch impurity by empirical branch probability for every possible split
-        #CODE...
+    # CODE...
 
 
 def main():
     load_data()
-    #HTree.start_tree()
+    # HTree.start_tree()
     global data
     class_labels = data["class"]
     global classes
+
+    # TODO use determine labels here
     for c in class_labels:
         if len(classes) == 1 and classes[0] != c:
             classes.append(c[1])
             break
         else:
             classes.append(c[1])
+
     classA = classes[0]
     classB = classes[1]
 
+    # TEST CODE -------------------------
+    test = data.iloc[range(0, 20)]
+    # print(test)
+    # print(test.shape)
+
+    print("determineLabels")
+    labels = determineLabels(test["cap-shape"])
+    print(labels)
+    print("instances")
+    print(countInstances(test, "cap-shape", labels))
+    # --------------------------------------
+
+    # TODO Change input! range(0, 10) just for testing purposes
+    # impurity(data.iloc[range(0, 10)])
+
+    # TODO STYLE, uniform naming style for functions. No conform atm
 
 
 main()
