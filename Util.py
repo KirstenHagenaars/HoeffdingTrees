4197fdb007f32bbb3b46dce1f73b918aa05a6215
KirstenHagenaars <49717068+KirstenHagenaars@users.noreply.github.com>
2019-12-19 11:13:21 +0100

    opschonen

---
+++ Util.py
@@ -0,0 +1,68 @@
+def giniIndex(total, inClassA, inClassB):
+    gini = 1 - ((inClassA / total) ** 2 + (inClassB / total) ** 2)
+    if 0 <= gini <= 1:
+        return gini
+    else:
+        print("Gini index problem")
+        return 0
+
+
+def determineLabels(array):  # Finds the different classes of an attribute
+    seen = []
+
+    for label in array:
+        there = False
+        for s in seen:
+            if s == label[1]:
+                there = True
+        if not there:
+            seen.append(label[1])
+    return seen
+
+
+# TODO test/fix this function, it works for odd numbers of labels, there are duplicates when the nr of labels is even
+def allSplits(labels):
+    combis = []
+    # when the labels are ['x', 'b', 's', 'f'], we do not want [['x', 'b', 's', 'f'],[]] in combis since this is not a split, right?
+    for i in range(1, ceil((len(labels) + 1) / 2)):
+        comb = combinations(labels, i)
+        for j in comb:
+            combine = [list(j)]
+            print(j)
+            partB = []
+            for label in labels:
+                seen = False
+                for p in j:
+                    if p == label:
+                        seen = True
+                if not seen:
+                    partB.append(label)
+            combine.append(partB)
+            combis.append(combine)
+    return combis
+
+
+def countInstances(instances, attribute, labels):  # Counts the number of instances of diff.classes within an attribute
+    counted = [0 for x in range(0, len(labels))]
+
+    for i in range(0, len(instances)):
+        for label in labels:
+            index = labels.index(label)
+            if instances.iloc[i][attribute][1] == label:
+                counted[index] = counted[index] + 1
+                break
+    return counted
+
+
+def updateBestGini(best, second_best, new):
+    if new[0] < best[0]:
+        return new, best
+    if new[0] < second_best[0]:
+        return best, new
+    else:
+        return best, second_best
+
+
+def epsilon(n, R, delta):
+    return math.sqrt(pow(R, 2) * math.log1p(1 / delta) / 2 * n)
+
--- main.py
+++ main.py
@@ -14,75 +14,6 @@ def loadData():
     data = pd.read_csv("dataset_24_mushroom.csv")  # panda dataframe
 
 
-def giniIndex(total, inClassA, inClassB):
-    gini = 1 - ((inClassA / total) ** 2 + (inClassB / total) ** 2)
-    if 0 <= gini <= 1:
-        return gini
-    else:
-        print("Gini index problem")
-        return 0
-
-
-def determineLabels(array):  # Finds the different classes of an attribute
-    seen = []
-
-    for label in array:
-        there = False
-        for s in seen:
-            if s == label[1]:
-                there = True
-        if not there:
-            seen.append(label[1])
-    return seen
-
-
-# TODO test/fix this function, it works for odd numbers of labels, there are duplicates when the nr of labels is even
-def allSplits(labels):
-    combis = []
-    # when the labels are ['x', 'b', 's', 'f'], we do not want [['x', 'b', 's', 'f'],[]] in combis since this is not a split, right?
-    for i in range(1, ceil((len(labels) + 1) / 2)):
-        comb = combinations(labels, i)
-        for j in comb:
-            combine = [list(j)]
-            print(j)
-            partB = []
-            for label in labels:
-                seen = False
-                for p in j:
-                    if p == label:
-                        seen = True
-                if not seen:
-                    partB.append(label)
-            combine.append(partB)
-            combis.append(combine)
-    return combis
-
-
-def countInstances(instances, attribute, labels):  # Counts the number of instances of diff.classes within an attribute
-    counted = [0 for x in range(0, len(labels))]
-
-    for i in range(0, len(instances)):
-        for label in labels:
-            index = labels.index(label)
-            if instances.iloc[i][attribute][1] == label:
-                counted[index] = counted[index] + 1
-                break
-    return counted
-
-
-def updateBestGini(best, second_best, new):
-    if new[0] < best[0]:
-        return new, best
-    if new[0] < second_best[0]:
-        return best, new
-    else:
-        return best, second_best
-
-
-def epsilon(n, R, delta):
-    return math.sqrt(pow(R, 2) * math.log1p(1 / delta) / 2 * n)
-
-
 # TODO finish function
 def impurity(instances, labels):  # labels off 'class', found in the main
 
@@ -90,8 +21,8 @@ def impurity(instances, labels):  # labels off 'class', found in the main
     counted = countInstances(instances, "class", labels)
 
     # Compute current gini index
-    currentGini = giniIndex(len(instances), counted[0], counted[1])                             #line 3 psuedocode
-    if (currentGini != 0):                                                                      # line 15 of psuedocode, gini = 0 if all instances are of the same class
+    currentGini = giniIndex(len(instances), counted[0], counted[1])  # line 3 psuedocode
+    if (currentGini != 0):  # line 15 of psuedocode, gini = 0 if all instances are of the same class
         # Test all possible splits
         best, second_best = [1, None], [1, None]  # first element is the gini, second element is the corresponding split
         for attribute in instances.columns:
@@ -131,7 +62,7 @@ def main():
     instances = countInstances(test, "cap-shape", labels)
     print(instances)
 
-    #I start following the pseudocode from here:
+    # I start following the pseudocode from here:
     n = []
     for c in classes:
         per_class = []
@@ -154,6 +85,7 @@ def main():
     # --------------------------------------
 
     # TODO Change input! range(0, 10) just for testing purposes
-    #impurity(data.iloc[range(0, 10)], classes)
+    # impurity(data.iloc[range(0, 10)], classes)
+
 
 main()
